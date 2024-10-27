8c29ce62eaffae279bc300e8b063014a5e5d323e
Jonathan Paul Moyonero Espinoza <moyonero@gmail.com>
2024-10-27 13:10:51 +0100

    Add new custom errors

---
+++ internal/custom-errors/conflict_error.go
@@ -0,0 +1,21 @@
+package custom_errors
+
+import (
+	"fmt"
+)
+
+type ConflictError struct {
+	CustomError
+}
+
+func (e ConflictError) Error() string {
+	return e.Message
+}
+
+// BuildConflictError Builds a ConflictError with the supplied message, and the corresponding code.
+func BuildConflictError(message string, args ...any) ConflictError {
+	if len(args) == 0 {
+		return ConflictError{CustomError{Message: message}}
+	}
+	return ConflictError{CustomError{Message: fmt.Sprintf(message, args...)}}
+}
