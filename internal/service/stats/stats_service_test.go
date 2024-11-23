bc40172ce0ba55951bda629c2a23b554d5ff9624
Jonathan Paul Moyonero Espinoza <moyonero@gmail.com>
2024-11-23 13:55:20 +0100

    Add GetFatHistory service unit tests

--- internal/service/stats/stats_service_test.go
+++ internal/service/stats/stats_service_test.go
@@ -125,6 +125,90 @@ var _ = Describe("SERVICE: Stats Test Suite", func() {
 
 	})
 
+	Context("Get Fat History", func() {
+
+		var (
+			ctxLogger *log.Entry
+			userID    string
+			months    int32
+			user      userDAO.User
+		)
+
+		BeforeEach(func() {
+			ctxLogger = toolsLogging.BuildLogger()
+
+			userID = "admin"
+			months = 3
+
+			user = userDAO.User{
+				ID:    "admin",
+				Email: "admin@admin.com",
+				Name:  "John",
+				FatHistory: []userDAO.FatHistory{
+					{
+						UserID: "admin",
+						Date:   parseTime("2024-11-01T10:30:00"),
+						Fat:    79.0,
+					},
+					{
+						UserID: "admin",
+						Date:   parseTime("2024-11-07T10:30:00"),
+						Fat:    80.5,
+					},
+					{
+						UserID: "admin",
+						Date:   parseTime("2024-11-14T10:30:00"),
+						Fat:    83.0,
+					},
+				},
+			}
+		})
+
+		It("CASE: Successful get fat history", func() {
+
+			mockUserDAO.EXPECT().GetUserWithFatHistory(userID, months, ctxLogger).
+				Times(1).
+				Return(&user, nil)
+
+			response, err := service.GetFatHistory(userID, months, ctxLogger)
+			Expect(err).To(BeNil())
+			Expect(len(response.Days)).To(Equal(3))
+			Expect(response.Days[0].Date).To(Equal("2024-11-01"))
+			Expect(response.Days[0].Value).To(Equal(float32(79)))
+			Expect(response.Days[1].Date).To(Equal("2024-11-07"))
+			Expect(response.Days[1].Value).To(Equal(float32(80.5)))
+			Expect(response.Days[2].Date).To(Equal("2024-11-14"))
+			Expect(response.Days[2].Value).To(Equal(float32(83)))
+		})
+
+		It("CASE: Successful retrieval without fat history info", func() {
+
+			user.FatHistory = nil
+
+			mockUserDAO.EXPECT().GetUserWithFatHistory(userID, months, ctxLogger).
+				Times(1).
+				Return(&user, nil)
+
+			response, err := service.GetFatHistory(userID, months, ctxLogger)
+			Expect(err).To(BeNil())
+			Expect(len(response.Days)).To(Equal(0))
+		})
+
+		It("CASE: Get fat history failed cause user not exist", func() {
+
+			user.WeightHistory = nil
+
+			mockUserDAO.EXPECT().GetUserWithFatHistory(userID, months, ctxLogger).
+				Times(1).
+				Return(nil, customErrors.BuildNotFoundError("not found"))
+
+			response, err := service.GetFatHistory(userID, months, ctxLogger)
+			Expect(err).To(BeAssignableToTypeOf(customErrors.NotFoundError{}))
+			Expect(response).To(BeNil())
+		})
+
+	})
+
 })
 
 func parseTime(dateStr string) time.Time {
