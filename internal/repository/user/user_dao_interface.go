b1d3db471ebd07d5dde569f060c55912781e2e41
Jonathan Paul Moyonero Espinoza <moyonero@gmail.com>
2024-11-23 12:31:38 +0100

    Add GetWeightHistory DAO implementation

--- internal/repository/user/user_dao_interface.go
+++ internal/repository/user/user_dao_interface.go
@@ -8,4 +8,5 @@ type IUserDAO interface {
 	GetUser(userID string, ctxLog *log.Entry) (*User, error)
 	GetUserByEmail(email string, ctxLog *log.Entry) (*User, error)
 	CreateUser(user *User, ctxLog *log.Entry) error
+	GetUserWithWeightHistory(userID string, months int32, ctxLog *log.Entry) (*User, error)
 }
