630180b26e88ee03908bcb90849ee101db3055c9
Jonathan Paul Moyonero Espinoza <moyonero@gmail.com>
2024-11-23 15:07:04 +0100

    Add GetStreakCalendar service unit tests

--- internal/service/stats/stats_service_test.go
+++ internal/service/stats/stats_service_test.go
@@ -209,6 +209,92 @@ var _ = Describe("SERVICE: Stats Test Suite", func() {
 
 	})
 
+	Context("Get Streak Calendar By Year And Month", func() {
+
+		var (
+			ctxLogger *log.Entry
+			userID    string
+			month     int32
+			year      int32
+			user      userDAO.User
+		)
+
+		BeforeEach(func() {
+			ctxLogger = toolsLogging.BuildLogger()
+
+			userID = "admin"
+			year = 2024
+			month = 11
+
+			user = userDAO.User{
+				ID:         "admin",
+				Email:      "admin@admin.com",
+				Name:       "John",
+				Streak:     77,
+				WeeklyGoal: 3,
+				GymAttendance: []userDAO.GymAttendance{
+					{
+						UserID: "admin",
+						Date:   parseTime("2024-11-01T10:30:00"),
+					},
+					{
+						UserID: "admin",
+						Date:   parseTime("2024-11-07T10:30:00"),
+					},
+					{
+						UserID: "admin",
+						Date:   parseTime("2024-11-14T10:30:00"),
+					},
+				},
+			}
+		})
+
+		It("CASE: Successful get streak calendar info", func() {
+
+			mockUserDAO.EXPECT().GetUserWithAttendance(userID, year, month, ctxLogger).
+				Times(1).
+				Return(&user, nil)
+
+			response, err := service.GetStreakCalendarByYearAndMonth(userID, year, month, ctxLogger)
+			Expect(err).To(BeNil())
+			Expect(response.Streak).To(Equal(int32(77)))
+			Expect(response.WeeklyGoal).To(Equal(int32(3)))
+			Expect(len(response.Days)).To(Equal(3))
+			Expect(response.Days[0]).To(Equal("2024-11-01"))
+			Expect(response.Days[1]).To(Equal("2024-11-07"))
+			Expect(response.Days[2]).To(Equal("2024-11-14"))
+		})
+
+		It("CASE: Successful retrieval without attendance info", func() {
+
+			user.GymAttendance = nil
+
+			mockUserDAO.EXPECT().GetUserWithAttendance(userID, year, month, ctxLogger).
+				Times(1).
+				Return(&user, nil)
+
+			response, err := service.GetStreakCalendarByYearAndMonth(userID, year, month, ctxLogger)
+			Expect(err).To(BeNil())
+			Expect(response.Streak).To(Equal(int32(77)))
+			Expect(response.WeeklyGoal).To(Equal(int32(3)))
+			Expect(len(response.Days)).To(Equal(0))
+		})
+
+		It("CASE: Get streak calendar failed cause user not exist", func() {
+
+			user.WeightHistory = nil
+
+			mockUserDAO.EXPECT().GetUserWithAttendance(userID, year, month, ctxLogger).
+				Times(1).
+				Return(nil, customErrors.BuildNotFoundError("not found"))
+
+			response, err := service.GetStreakCalendarByYearAndMonth(userID, year, month, ctxLogger)
+			Expect(err).To(BeAssignableToTypeOf(customErrors.NotFoundError{}))
+			Expect(response).To(BeNil())
+		})
+
+	})
+
 })
 
 func parseTime(dateStr string) time.Time {
