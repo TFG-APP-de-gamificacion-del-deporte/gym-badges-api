438ad79e89b469ba7f9b8de9d36251cc795a998b
DiegoRevenga <diegorevengagonzalez@gmail.com>
2024-12-06 17:51:59 +0100

    Add AddWeight to stats_handler

--- internal/handler/stats/stats_handler_interface.go
+++ internal/handler/stats/stats_handler_interface.go
@@ -8,6 +8,7 @@ import (
 
 type IStatsHandler interface {
 	GetWeightHistory(params stats.GetWeightHistoryByUserIDParams) middleware.Responder
+	AddWeight(params stats.AddWeightParams) middleware.Responder
 	GetFatHistory(params stats.GetFatHistoryByUserIDParams) middleware.Responder
 	GetStreakCalendar(params stats.GetStreakCalendarByUserIDParams) middleware.Responder
 }
