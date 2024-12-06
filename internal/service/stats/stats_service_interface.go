d9f2474f5afd246a965e2245a5df21e37f36e5d7
DiegoRevenga <diegorevengagonzalez@gmail.com>
2024-12-06 19:09:30 +0100

    Add AddBodyFat to stats_service

--- internal/service/stats/stats_service_interface.go
+++ internal/service/stats/stats_service_interface.go
@@ -10,5 +10,6 @@ type IStatsService interface {
 	GetWeightHistory(userID string, months int32, ctxLog *log.Entry) (*models.MeasurementHistoryResponse, error)
 	AddWeight(userID string, weight float32, ctxLog *log.Entry) error
 	GetFatHistory(userID string, months int32, ctxLog *log.Entry) (*models.MeasurementHistoryResponse, error)
+	AddBodyFat(userID string, fat float32, ctxLog *log.Entry) error
 	GetStreakCalendarByYearAndMonth(userID string, year int32, month int32, ctxLog *log.Entry) (*models.StreakCalendarResponse, error)
 }
