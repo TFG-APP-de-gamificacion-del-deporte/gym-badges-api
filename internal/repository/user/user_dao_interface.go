3e5958cb4471f2e4f0971c287e7bff766dcf871e
DiegoRevenga <diegorevengagonzalez@gmail.com>
2024-12-07 11:27:14 +0100

    Add AddGymAttendance to user_dao

--- internal/repository/user/user_dao_interface.go
+++ internal/repository/user/user_dao_interface.go
@@ -10,11 +10,16 @@ type IUserDAO interface {
 	GetUser(userID string, ctxLog *log.Entry) (*User, error)
 	GetUserByEmail(email string, ctxLog *log.Entry) (*User, error)
 	CreateUser(user *User, ctxLog *log.Entry) error
+
 	GetUserWithWeightHistory(userID string, months int32, ctxLog *log.Entry) (*User, error)
 	AddWeight(userID string, weight float32, date time.Time, ctxLog *log.Entry) error
+
 	GetUserWithFatHistory(userID string, months int32, ctxLog *log.Entry) (*User, error)
 	AddBodyFat(userID string, bodyFat float32, date time.Time, ctxLog *log.Entry) error
+
 	GetUserWithAttendance(userID string, year int32, month int32, ctxLog *log.Entry) (*User, error)
+	AddGymAttendance(userID string, date time.Time, ctxLog *log.Entry) error
+
 	GetUserWithFriends(userID string, offset int32, size int32, ctxLog *log.Entry) (*User, error)
 	GetUserWithBadges(userID string, ctxLog *log.Entry) (*User, error)
 	AddFriend(userID string, friendID string, ctxLog *log.Entry) (*User, error)
